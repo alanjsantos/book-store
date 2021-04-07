d56156375457a5b017b70d840279edb5e4ca72a8
Alan Jose dos Santos <alan.santos@pampa.compasso>
2021-04-07 15:05:56 -0300

    AJuste aplication main

--- src/main/java/com/bookstore/system/SystemApplication.java
+++ src/main/java/com/bookstore/system/SystemApplication.java
@@ -2,18 +2,12 @@ package com.bookstore.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableWebMvc
-public class SystemApplication implements WebMvcConfigurer {
+public class SystemApplication  {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(SystemApplication.class, args);
 	}
